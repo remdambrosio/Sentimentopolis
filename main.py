845feebe2d4d4a693c9f643bcc7c178b3bc3eb39
remdambrosio <rda7@shaw.ca>
2024-12-18 12:05:01 -0800

    prepared for different analysis types, added nsfwness

--- main.py
+++ main.py
@@ -13,44 +13,53 @@ def main():
     Main function
     """
     parser = argparse.ArgumentParser(description='Does PRAW stuff')
+
     parser.add_argument('-pu', '--pull', action='store_true', help='pull new data from PRAW')
+    parser.add_argument('-ps', '--pull_subreddit', type=str, help='subreddit to pull', default='DestinyTheGame')
+    parser.add_argument('-pt', '--pull_type', type=str, help='filter type: top/hot/new', default='top')
+    parser.add_argument('-pc', '--pull_count', type=int, help='number of posts to pull', default=100)
+    parser.add_argument('-pi', '--pull_time', type=str, help='time frame: day/month/year/all', default='year')
+
     parser.add_argument('-an', '--analyze', action='store_true', help='analyze data from file')
+    parser.add_argument('-aa', '--analyze_attribute', type=str, help='analysis attribute: sentiment, nsfwness', default='sentiment')
+
     parser.add_argument('-re', '--report', action='store_true', help='write report on results')
+
     args = parser.parse_args()
 
     if args.pull:
-        pull()
+        pull(subreddit=args.pull_subreddit, type=args.pull_type, count=args.pull_count, time=args.pull_time)
 
     if args.analyze:
-        analyze()
+        analyze(attribute=args.analyze_attribute)
 
     if args.report:
         report()
 
 
-def pull():
+def pull(subreddit, type, count, time):
     """
     Pulls data
     """
     puller = Puller()
     print('Pulling data...')
-    puller.pull_posts('DestinyTheGame', pull_type='top', post_count=100, time_frame='year')
+    puller.pull_posts(subreddit_name=subreddit, pull_type=type, post_count=count, time_frame=time)
     print('...Data pulled...')
     puller.write_posts_json('data/raw/posts.json')
     print('...Data written to file.')
     return
 
 
-def analyze():
+def analyze(attribute):
     """
     Analyzes data
     """
     print('Analyzing data...')
-    analyzer = Analyzer(in_path='data/raw/posts.json', use_gpu=True)
-    analyzer.trajectory_analysis()
-    print('...Sentiment gathered...')
+    analyzer = Analyzer(in_path='data/raw/posts.json', analysis_attribute=attribute, use_gpu=True)
+    analyzer.trajectory_analysis(score_threshold=1)
+    print('...Analysis gathered...')
     analyzer.write_results_json('data/processed/results.json')
-    print('...Results written to file.')
+    print('...Analysis written to file.')
     return
 
 
